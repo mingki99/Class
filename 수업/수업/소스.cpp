decd4331a07fd664c2abacd3f5adfbc1227125ab
mang <dkwnajsquf@naver.com>
2021-09-29 09:48:24 +0900

    학원

---
+++ 수업/수업/소스.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void Star();
+
+int main()
+{
+	//int a [100]
+
+		
+
+	//for (int a; a =< 100; a++)
+		
+	for (int a = 0; a < 5; a++)
+	{
+		
+		for (int b = 0; b < a; b++)
+		{
+			
+			char c = '*';
+			c = a;
+			cout << a << endl;
+			
+		}
+	
+		
+	}
+
+	return 0;
+}
+
---
+++ 수업전 공부/수업전 공부/소스.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdlib>
+
+
+
+using namespace std;
+
+int Array();
+
+void Shuffle(int a);
+
+void Render();
+
+
+int main()
+{
+	Array();
+	Shuffle(a);
+	Render();
+
+}
+
+int Array();
+{
+
+	int Number[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	
+	return Number[10];
+}
+
+
+void Shuffle(int a)
+{
+	a = a[index1]
+	
+	a[index1] = rand % 10;
+	a[index2] = rand % 10;
+
+	int b;
+
+	 b = Number[index1]
+	Number[index2]= Number[index1]
+	Number[index2] = b
+
+
+
+}
+
+void Render()
+{
+
+}
+
