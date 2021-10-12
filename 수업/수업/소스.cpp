466ceaa6b3c025eea0c3100857801ff43b56b4aa
mang <dkwnajsquf@naver.com>
2021-10-12 09:52:29 +0900

    수업

--- 수업/수업/소스.cpp
+++ 수업/수업/소스.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
-
+#include "engin.h"
 using namespace std;
-
-class Player;
-
-void move();
-
-void (int a, Player* Playerpointer)
-{
-	Playerpointer-> X
-
-}
-
 
 int main()
 {
 	
+
+
 
 
 	return 0;
