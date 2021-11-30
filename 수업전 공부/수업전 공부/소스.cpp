e9c359bda1623156608140c6ba69dac2ef449af8
mang <dkwnajsquf@naver.com>
2021-11-30 12:40:17 +0900

    알고리즘 공부 X 초급 알고리즘 공부하자

--- 수업전 공부/수업전 공부/소스.cpp
+++ 수업전 공부/수업전 공부/소스.cpp
@@ -1,73 +1,76 @@
 ﻿#include <iostream>
-#include<vector>
+#include<sstream>
+#include<algorithm>
 
-#include "Character.h"
-#include"Boar.h"
-#include"Goblin.h"
-#include"Player.h"
-#include"Slime.h"
-#include "Monster.h"
 
-#include<time.h>
-#include<stdlib.h>
 using namespace std;
 
+template <typename T>
+class dynamic_array
+{
+	T* data;
+	size_t n;
 
+	//배열 크기를 인자로 받는 생성자 추가
+public:
+	dynamic_array(int n)
+	{
+		this->n = n;
+		data = new T[n];
+	}
+
+
+	//복사 생성자 추가
+	dynamic_array(const dynamic_array<T>& other)
+	{
+		n = other.n;
+		data = new T[n];
+
+		for (int i = 0; i < n; i++)
+		{
+			data[i] = other[i];
+		}
+	}
+
+	T& operator[](int index)
+	{
+		return data[index];
+	}
+
+	{
+		const T& operator[] (int index) const
+		{
+			return index;
+		}
+
+		T& at(int index)
+		{
+			if (index < n)
+				return data[index];
+			throw " index Out of range";
+		}
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+}
 
 
 int main()
 {
 	
-	
-	
-	vector<UCharacter*> Characters; 
-	// UCharacter 의 포인터를 지정하고 변수를 지정해주무로써 
-	// 변수안에 vector 형의 UCharacter 의 값이 들어간다.
-
-
-	Characters.push_back(new PPlayer());
-	//먼져 새로운 Plat 클래스 안에 UCharacter 의 값이 들어간다?
-
-
-	srand(time(nullptr));
-	//랜덤
-
-	for (int i = 0; i < 10; i++)
-	{
-		int Type = rand() % 3;
-
-		if (Type == 0)
-		{
-			Characters.push_back(new PBoar());
-		}
-		else if (1 == Type)
-		{
-			Characters.push_back(new PGoblin());
-		}
-		else
-		{
-			Characters.push_back(new PSlime());
-
-		}
-	}
-
-
-	for (size_t i = 0; i < Characters.size(); ++i)
-	{
-		Characters[i] -> move();
-	}
-	for (UCharacter* PlayCharacters : Characters)
-	{
-		PlayCharacters->move();
-	}
-	for (auto iter = Characters.begin(); iter != Characters.end(); ++iter)
-	{
-		(*iter)->move();
-	}
-	//PGoblin* goblin = new PGoblin();
-
-	//delete goblin;
-
 	return 0;
 }
 
